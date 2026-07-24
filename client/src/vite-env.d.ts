0015507bef835b88c580ca7e369784f30905d710
praju120056 <praju2005x86@gmail.com>
2026-07-25 02:33:44 +0530

    type safety update

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// \u003creference types="vite/client" /\u003e
 
 interface ImportMetaEnv {
-    readonly VITE_API_URL: string
+    readonly VITE_API_URL: string;
+    readonly VITE_GOOGLE_CLIENT_ID?: string;
 }
 
 interface ImportMeta {
